dd014d34b81abad1f3da5b6f3a99726b53023433
guptan0506 <navyag0509@gmail.com>
2026-09-15 13:27:46 -0400

    Add project files.

---
+++ Lab3/Form1.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace Lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lstProducts = new ListBox();
+            SuspendLayout();
+            // 
+            // lstProducts
+            // 
+            lstProducts.FormattingEnabled = true;
+            lstProducts.Location = new Point(0, 0);
+            lstProducts.Name = "lstProducts";
+            lstProducts.Size = new Size(240, 164);
+            lstProducts.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lstProducts);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ListBox lstProducts;
+    }
+}
---
+++ Lab3/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab3
+{
+    public class Product
+    {
+        public Product() { }
+
+        public Product(string code, string desc, decimal price)
+        {
+            Code = code;
+            Description = desc;
+            Price = price;
+        }
+
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}

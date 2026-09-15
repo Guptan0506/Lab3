24e2cd971d69c7dc4510d0b016da14d4bd416d97
guptan0506 <navyag0509@gmail.com>
2026-09-15 13:46:52 -0400

    Added Book class

---
+++ Lab3/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab3
+{
+    // Book : Product means that Book class is inheriting from Product class
+    public class Book : Product
+    {
+        public string Author { get; set; }
+
+        public Book() { }
+        
+        public Book(string code, string desc, decimal price, string author) : base(code, desc, price)
+        {
+            Author = author;
+        }
+
+        public override string GetDisplayText(string sep)
+        {
+            // Concatenates Author to the existing text
+            return base.GetDisplayText(sep) + sep + $"{Author}";
+        }
+    }
+}
--- Lab3/Product.cs
+++ Lab3/Product.cs
@@ -18,5 +18,12 @@ namespace Lab3
         public string Code { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
+
+        public virtual string GetDisplayText(string sep)
+        {
+            // returns a string in the following order but since price is a 
+            // decimal it needs to be converted to string in currency format.
+            return $"{Code}{sep}{Description}{sep}{Price.ToString("c")}";
+        }
     }
 }

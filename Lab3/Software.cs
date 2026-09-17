d5778d3e45c4d6d5cd754f6340aa9b9818566cb1
guptan0506 <navyag0509@gmail.com>
2026-09-17 12:53:32 -0400

    Added Software.cs

---
+++ Lab3/Software.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab3
+{
+    public class Software : Product
+    {
+        public Software() { }
+        public Software(string code, string desc, decimal price, string company) : base(code, desc, price)
+        {
+            Company = company;
+        }
+
+        public string Company { get; set; }
+
+        public override string GetDisplayText(string sep)
+        {
+            return base.GetDisplayText(sep) + sep + $"{Company}";
+        }
+    }
+}
